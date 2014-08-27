80bdd832f817e4e678bc4d93facb8e216daa3f57
J. Matt Peterson <jmatt@jmatt.org>
2014-08-27 12:55:36 -0700

    Ignore scripts directory. 	modified:   .gitignore 	deleted:    scripts/rm_all_pyc.sh 	deleted:    scripts/set_permissions.sh

--- scripts/rm_all_pyc.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# this removes all pyc file under this location - recursively
-
-export STRATOSPHERE_HOME=/opt/dev/stratosphere
-
-find ${STRATOSPHERE_HOME} -name "*.pyc" -exec rm '{}' ';'
--- scripts/set_permissions.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-export RTWO_HOME=/opt/dev/dtwo
-
-chmod -R g+w ${RTWO_HOME}
-chown -R root:core-services ${RTWO_HOME}
-
